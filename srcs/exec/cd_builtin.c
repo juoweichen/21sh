c9d0840f069210e25af8fb3ed5e0d10bf0a1f198
juochen <pootitan@gmail.com>
2019-04-23 20:55:25 -0700

    hotfix cd 2

--- srcs/exec/cd_builtin.c
+++ srcs/exec/cd_builtin.c
@@ -17,25 +17,25 @@ void	update_pwd(t_exec *exec, char *next_dir)
 	char	*pwd;
 	char	*oldpwd;
 
-	if ((pwd = dict_get(exec->com_dict, "PWD")) == NULL ||
-		(oldpwd = dict_get(exec->com_dict, "OLDPWD")) == NULL ||
+	if ((pwd = dict_get(exec->env_dict, "PWD")) == NULL ||
+		(oldpwd = dict_get(exec->env_dict, "OLDPWD")) == NULL ||
 		next_dir == NULL)
 		return ;
-	dict_add(exec->com_dict, "OLDPWD", pwd, ft_strlen(pwd));
-	dict_add(exec->com_dict, "PWD", next_dir, ft_strlen(next_dir));
+	dict_add(exec->env_dict, "OLDPWD", pwd, ft_strlen(pwd));
+	dict_add(exec->env_dict, "PWD", next_dir, ft_strlen(next_dir));
 }
 
 void	cd_builtin(t_exec *exec, char **arg)
 {
 	char	*next_dir;
 	char	*com_path;
-	
+
 	if (arg[1] == NULL && (com_path = dict_get(exec->env_dict, "HOME")) != NULL)
 		next_dir = ft_strdup(com_path);
 	else if (arg[2] == NULL)
 	{
 		if (ft_strequ(arg[1], "-") &&
-			(com_path = dict_get(exec->com_dict, "OLDPWD")) != NULL)
+			(com_path = dict_get(exec->env_dict, "OLDPWD")) != NULL)
 			next_dir = ft_strdup(com_path);
 		else if (arg[1][0] == '/')
 			next_dir = ft_strdup(arg[1]);
